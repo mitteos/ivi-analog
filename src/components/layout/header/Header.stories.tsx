fcd0f97e168cdb89eaa1655978fc41102a869993
Bonamente <56506035@protonmail.com>
2023-04-17 14:25:34 +0500

    feat: add Header

---
+++ src/components/layout/header/Header.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Header from './Header';
+
+const meta: Meta<typeof Header> = {
+  title: 'Components/Header',
+  component: Header,
+  tags: ['autodocs'],
+  argTypes: {
+    handleHovering: () => {},
+    handleClick: () => {},
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Header>;
+
+export const Base: Story = {
+  args: {},
+};
--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,7 +1,9 @@
+import HeaderContent from './header-content/HeaderContent';
+
 const Header: React.FC = () => {
   return (
-    <header className='text-white text-2xl bg-slate-800'>
-      <div className='page-container h-24 flex items-center'>Header</div>
+    <header className='text-white page-bg-color'>
+      <HeaderContent />
     </header>
   );
 };
