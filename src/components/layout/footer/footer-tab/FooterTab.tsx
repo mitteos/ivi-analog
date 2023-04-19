023991bea8fee4a40b8a57258657648d8350c173
Bonamente <56506035@protonmail.com>
2023-04-19 09:45:50 +0500

    feat: add FooterTab

---
+++ src/components/layout/footer/footer-tab/FooterTab.tsx
@@ -0,0 +1,27 @@
+import { FooterTabProps } from './types';
+
+const FooterTab: React.FC<FooterTabProps> = ({
+  isActive,
+  handleClick,
+  children,
+  ...attributes
+}) => {
+  const text = isActive ? 'text-white' : 'text-white/[.48]';
+  const opacity = isActive ? 'opacity-100' : ' opacity-0';
+
+  return (
+    <button
+      className={`footer-tab ${text}`}
+      type='button'
+      onClick={handleClick ? handleClick : undefined}
+      {...attributes}
+    >
+      <div
+        className={`absolute top-0 left-[50%] -translate-x-[50%] w-16 h-12  bg-[url("/tabBarGlow.svg")] bg-auto ${opacity}`}
+      ></div>
+      {children}
+    </button>
+  );
+};
+
+export default FooterTab;
