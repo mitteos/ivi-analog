ced67169f0a463f30ea9295a686c57f635c642d5
Bonamente <56506035@protonmail.com>
2023-04-21 17:08:58 +0500

    refactor(Socials): remove duplicate code

--- src/components/layout/footer/socials/Socials.tsx
+++ src/components/layout/footer/socials/Socials.tsx
@@ -1,61 +1,29 @@
-const Socials = () => {
+import Image from 'next/image';
+import { SocialsProps } from './types';
+
+const Socials: React.FC<SocialsProps> = ({ links }) => {
   return (
-    <div className='flex items-center [&>*:not(:first-child)]:ml-3'>
-      <a
-        className='footer-social'
-        href='https://vk.com/iviru?crc=fa4448c13e06e69ba9e814e8743c7e2e'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        <span className='sr-only'>VK</span>
-        <img className='footer-social-img' src='/vkontakte-logo.svg' />
-      </a>
-      <a
-        className='footer-social'
-        href='https://ok.ru/ivi.ru'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        <span className='sr-only'>OK</span>
-        <img className='footer-social-img' src='/odnoklassniki-logo.svg' />
-      </a>
-      <a
-        className='footer-social'
-        href='https://twitter.com/ivi_ru'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        <span className='sr-only'>Twitter</span>
-        <img className='footer-social-img' src='/twitter-logo.svg' />
-      </a>
-      <a
-        className='footer-social'
-        href='https://vb.me/a0544c'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        <span className='sr-only'>Viber</span>
-        <img className='footer-social-img' src='/viber-logo.svg' />
-      </a>
-      <a
-        className='footer-social'
-        href='https://www.linkedin.com/company/2543415/'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        <span className='sr-only'>LinkedIn</span>
-        <img className='footer-social-img' src='/linkedin-logo.svg' />
-      </a>
-      <a
-        className='footer-social'
-        href='https://t.me/official_iviru'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        <span className='sr-only'>Telegram</span>
-        <img className='footer-social-img' src='/telegram-logo.svg' />
-      </a>
-    </div>
+    <ul className='flex items-center [&>*:not(:first-child)]:ml-3'>
+      {links.map((link) => (
+        <li key={link.text}>
+          <a
+            className='footer-social'
+            href={link.path}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <span className='sr-only'>{link.text}</span>
+            <Image
+              className='footer-social-img'
+              width={16}
+              height={16}
+              src={link.imgUrl || ''}
+              alt={`${link.text} logo`}
+            />
+          </a>
+        </li>
+      ))}
+    </ul>
   );
 };
 
