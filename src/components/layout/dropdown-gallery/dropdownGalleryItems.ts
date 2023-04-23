fcee1a71f12808a85fb5eb6dfbcfefcaeb53787e
Bonamente <56506035@protonmail.com>
2023-04-23 21:30:22 +0500

    feat: add DropdownGallery

---
+++ src/components/layout/dropdown-gallery/dropdownGalleryItems.ts
@@ -0,0 +1,57 @@
+export const dropdownGalleryItems = [
+  '/images/dropdown-gallery/1.jpeg',
+
+  '/images/dropdown-gallery/2.jpeg',
+
+  '/images/dropdown-gallery/3.jpeg',
+
+  '/images/dropdown-gallery/4.jpeg',
+
+  '/images/dropdown-gallery/5.jpeg',
+
+  '/images/dropdown-gallery/6.jpeg',
+
+  '/images/dropdown-gallery/7.jpeg',
+
+  '/images/dropdown-gallery/8.jpeg',
+
+  '/images/dropdown-gallery/9.jpeg',
+
+  '/images/dropdown-gallery/10.jpeg',
+
+  '/images/dropdown-gallery/11.jpeg',
+
+  '/images/dropdown-gallery/12.jpeg',
+
+  '/images/dropdown-gallery/13.jpeg',
+
+  '/images/dropdown-gallery/14.jpeg',
+
+  '/images/dropdown-gallery/15.jpeg',
+
+  '/images/dropdown-gallery/16.jpeg',
+
+  '/images/dropdown-gallery/17.jpeg',
+
+  '/images/dropdown-gallery/18.jpeg',
+
+  '/images/dropdown-gallery/19.jpeg',
+
+  '/images/dropdown-gallery/20.jpeg',
+
+  '/images/dropdown-gallery/21.jpeg',
+
+  '/images/dropdown-gallery/22.jpeg',
+
+  '/images/dropdown-gallery/23.jpeg',
+
+  '/images/dropdown-gallery/24.jpeg',
+
+  '/images/dropdown-gallery/25.jpeg',
+
+  '/images/dropdown-gallery/26.jpeg',
+
+  '/images/dropdown-gallery/27.jpeg',
+
+  '/images/dropdown-gallery/28.jpeg',
+];
