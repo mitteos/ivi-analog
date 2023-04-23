4a3dfe55cf24f9ffdac736e085e7e477a9746f56
Bonamente <56506035@protonmail.com>
2023-04-23 21:21:48 +0500

    feat: add Copyright

---
+++ src/components/layout/footer/copyright/Copyright.tsx
@@ -0,0 +1,44 @@
+const Copyright = () => {
+  return (
+    <div className='mt-8 text-[15px] text-white/[.48]'>
+      <p>©&nbsp;2023</p>
+      <p>
+        Developed by{' '}
+        <span>
+          <a
+            className='header-link'
+            href='https://github.com/Bonamente'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            bonamente
+          </a>
+        </span>
+        ,{' '}
+        <span>
+          <a
+            className='header-link'
+            href='https://github.com/kimutir'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            kimutir
+          </a>
+        </span>
+        ,{' '}
+        <span>
+          <a
+            className='header-link'
+            href='https://github.com/mitteos'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            mitteos
+          </a>
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default Copyright;
