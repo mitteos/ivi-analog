2d78293b08d9bd90b6f08c856ad33c61673a37e9
Bonamente <56506035@protonmail.com>
2023-04-23 21:05:17 +0500

    feat: add context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { usePreviousRoute } from '@/hooks/usePreviousRoute';
+import AppContext from '@/context/AppContext';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  const prevRoute = usePreviousRoute();
+
+  return (
+    <AppContext.Provider value={prevRoute}>
+      <Component {...pageProps} />
+    </AppContext.Provider>
+  );
 };
 
 export default App;
